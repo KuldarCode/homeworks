e80e989358ad285fc68e05c0cbdf07a51b3b4203
KuldarCode <kuldar.semmel@hotmail.com>
2020-11-18 13:06:19 +0200

    test

---
+++ homework9   A   .py
@@ -0,0 +1,13 @@
+
+
+def roundit( number ):
+    roundedNr = round(float(number))
+    print(roundedNr)
+    if roundedNr % 2 == 0:
+        print("True")
+    else:
+        print("False")
+roundit(1.8)
+roundit(1.1)
+roundit(5.6)
+roundit(17.6)
